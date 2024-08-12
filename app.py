831414161c90e17724cef44e379ebaf65f47e614
Gil <ghopecs@gmail.com>
2024-08-12 13:57:47 -0400

    add venv & flask app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from workout_functions.block_calc.main import calculate_workouts
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        try:
+            # Get the 1RM inputs from the form
+            squat_1rm = int(request.form['squat_1rm'])
+            bench_1rm = int(request.form['bench_1rm'])
+            deadlift_1rm = int(request.form['deadlift_1rm'])
+            ohp_1rm = int(request.form['ohp_1rm'])
+            
+            # Calculate the workout plan
+            orms = (squat_1rm, bench_1rm, deadlift_1rm, ohp_1rm)
+            workout_plan = calculate_workouts(orms)
+
+            # Pass the workout plan to the template
+            return render_template('index.html', workout_plan=workout_plan)
+        
+        except ValueError:
+            return render_template('index.html', error="Please enter valid integer values for all 1RMs.")
+    
+    # If GET request, just render the form
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
